442ab9c84935b7ba356333f6db71f8110d2de3a6
Alexandre Vessoni <av.vessoni@gmail.com>
2020-05-19 23:00:47 +0100

    Validate Data, Appointment Model, repositories, list Appointment

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,20 +1,33 @@
 import { Router } from 'express';
-import { uuid } from 'uuidv4';
+import { startOfHour, parseISO } from 'date-fns';
+
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 const appointmentsRouter = Router();
+const appointmentsRepository = new AppointmentsRepository();
 
-const appointments = [];
+appointmentsRouter.get('/', (request, response) => {
+  const appointments = appointmentsRepository.all();
+
+  return response.json(appointments);
+});
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
-  const appointment = {
-    id: uuid(),
-    provider,
-    date,
-  };
+  const parsedDate = startOfHour(parseISO(date));
 
-  appointments.push(appointment);
+  const findAppointmentInSameDate = appointmentsRepository.findByDate(
+    parsedDate,
+  );
+
+  if (findAppointmentInSameDate) {
+    return response
+      .status(400)
+      .json({ message: 'This appointment is already booked' });
+  }
+
+  const appointment = appointmentsRepository.create(provider, parsedDate);
 
   return response.json(appointment);
 });
