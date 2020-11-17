6608074d310178cfbb9c7fadd6ab3eac6ecefa21
Alexandre <av.vessoni@gmail.com>
2020-11-17 18:14:38 +0000

    Appointments database

--- src/repositories/AppointmentsRepository.ts
+++ src/repositories/AppointmentsRepository.ts
@@ -1,39 +1,21 @@
-import { isEqual } from 'date-fns';
+import { EntityRepository, Repository } from 'typeorm';
+
 import Appointment from '../models/Appointment';
 
-// Data Transfer Objects
-interface CreateAppointmentDTO {
-  provider: string;
-  date: Date;
-}
-
-class AppointmentsRepository {
+@EntityRepository(Appointment)
+class AppointmentsRepository extends Repository<Appointment> {
   private appointments: Appointment[];
 
-  constructor() {
-    this.appointments = [];
-  }
+  public async findByDate(date: Date): Promise<Appointment | null> {
+    // const findAppointment = this.appointments.find(appointment =>
+    //   isEqual(date, appointment.date),
+    // );
 
-  public all(): Appointment[] {
-    return this.appointments;
-  }
-
-  public findByDate(date: Date): Appointment | null {
-    const findAppointment = this.appointments.find(appointment =>
-      isEqual(date, appointment.date),
-    );
+    const findAppointment = await this.findOne({
+      where: { date },
+    });
 
     return findAppointment || null;
-  }
-
-  //
-
-  public create({ provider, date }: CreateAppointmentDTO): Appointment {
-    const appointment = new Appointment({ provider, date });
-
-    this.appointments.push(appointment);
-
-    return appointment;
   }
 }
 
