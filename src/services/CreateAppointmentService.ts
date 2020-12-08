031f6b5c5e7afa4c4bc171bcbb128e45c261a58c
Alexandre <av.vessoni@gmail.com>
2020-12-08 22:42:58 +0000

    Fix Highlight

--- src/services/CreateAppointmentService.ts
+++ src/services/CreateAppointmentService.ts
@@ -22,7 +22,7 @@ class CreateAppointmentService {
     );
 
     if (findAppointmentInSameDate) {
-      throw AppError('This appointment is already booked');
+      throw new AppError('This appointment is already booked');
     }
 
     const appointment = appointmentsRepository.create({
